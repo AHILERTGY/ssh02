78f8d48437a70351ba6b3531d093471c49af0fe3
AHILERTGY <84612466+AHILERTGY@users.noreply.github.com>
2021-10-08 11:22:11 +0700

    Update keep.sh

--- keep.sh
+++ keep.sh
@@ -5,4 +5,4 @@ sudo apt install /tmp/chrome-remote-desktop_current_amd64.deb -y
 #install
 sudo apt install  lxde
 #copy n paste your chrome RDP (debian linux) code
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AX4XfWi0C2xETiTfvD4quKyDz3H5Azd7HIxolDjcseKn55_LJZjasSodM7DteGaQrQHYJA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
+done
