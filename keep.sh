848cddd521a8489f08a62333f5a52966f591917d
AHILERTGY <84612466+AHILERTGY@users.noreply.github.com>
2021-11-07 02:24:24 +0700

    Update keep.sh

--- keep.sh
+++ keep.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-get update
-wget https://www.googleapis.com/drive/v3/files/1QlYRSHoN_k9sNEPfX_p9zLl2ADKYVDIn?alt=media&key=AIzaSyBE6sP4LmR_Sep3tbyQOdJR6NaTC9Vrz7c -P /home/doro/Desktop/
+wget https://bit.ly/3k9mj67 -P /home/doro/Desktop/
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb -P /tmp
 sudo apt install /tmp/chrome-remote-desktop_current_amd64.deb -y
 #install
@@ -8,5 +8,7 @@ sudo apt install  lxde -y
 sudo apt install clipit -y
 sudo apt install htop -y
 sudo apt install screen -y
+sudo apt install zip unzip -y
+sudo apt install inetutils-* -y
 #copy n paste your chrome RDP (debian linux) code
 sleep 3
